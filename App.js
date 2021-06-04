cf1eb6615b0a72387117e584dae67f6dff8ac767
HrDelwar <hrdelwar75@gmail.com>
2021-06-04 20:22:44 +0600

    add hotest food area

--- App.js
+++ App.js
@@ -118,7 +118,248 @@ export class App extends Component {
             </ImageBackground>
           </View>
         {/* end home banner */}
-          
+          {/* Hotest Food! */}
+        <View style={{ marginTop: 20, paddingHorizontal: 20 }}>
+            <Text
+              style={{
+                fontSize: 16,
+                color: "black",
+                fontWeight: "bold",
+              }}
+            >
+              HOTEST FOOD!,
+            </Text>
+            <Text
+              style={{
+                fontSize: 12,
+                color: "grey",
+                fontWeight: "bold",
+              }}
+            >
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod
+            </Text>
+          </View>
+          <ScrollView
+            horizontal={true}
+            style={{
+              paddingLeft: 20,
+              paddingRight: 10,
+              marginTop: 10,
+            }}
+            showsHorizontalScrollIndicator={false}
+          >
+            <View
+              style={{
+                flexDirection: "row",
+              }}
+            >
+              <View
+                style={{
+                  overflow: "hidden",
+                  borderRadius: 10,
+                  marginRight: 10,
+                  maxHeight: 200,
+                }}
+              >
+                <ImageBackground
+                  style={{
+                    width: 150,
+                    height: 200,
+                  }}
+                  source={{
+                    uri:
+                      "https://images.unsplash.com/photo-1478145046317-39f10e56b5e9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80",
+                  }}
+                >
+                  <View style={{ position: "absolute", bottom: 20, left: 20 }}>
+                    <Text
+                      style={{
+                        fontSize: 14,
+                        color: "white",
+                        fontWeight: "bold",
+                      }}
+                    >
+                      Lorem Ipsum
+                    </Text>
+                    <Text
+                      style={{
+                        fontSize: 14,
+                        color: "white",
+                        fontWeight: "bold",
+                      }}
+                    >
+                      <Feather name="star" size={18} color="white" /> 5
+                    </Text>
+                  </View>
+                </ImageBackground>
+              </View>
+              <View
+                style={{
+                  overflow: "hidden",
+                  borderRadius: 10,
+                  marginRight: 10,
+                  maxHeight: 200,
+                }}
+              >
+                <ImageBackground
+                  style={{
+                    width: 150,
+                    height: 200,
+                  }}
+                  source={{
+                    uri:
+                      "https://images.unsplash.com/photo-1506084868230-bb9d95c24759?ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80",
+                  }}
+                >
+                  <View style={{ position: "absolute", bottom: 20, left: 20 }}>
+                    <Text
+                      style={{
+                        fontSize: 14,
+                        color: "white",
+                        fontWeight: "bold",
+                      }}
+                    >
+                      Lorem Ipsum
+                    </Text>
+                    <Text
+                      style={{
+                        fontSize: 14,
+                        color: "white",
+                        fontWeight: "bold",
+                      }}
+                    >
+                      <Feather name="star" size={18} color="white" /> 5
+                    </Text>
+                  </View>
+                </ImageBackground>
+              </View>
+              <View
+                style={{
+                  overflow: "hidden",
+                  borderRadius: 10,
+                  marginRight: 10,
+                  maxHeight: 200,
+                }}
+              >
+                <ImageBackground
+                  style={{
+                    width: 150,
+                    height: 200,
+                  }}
+                  source={{
+                    uri:
+                      "https://images.unsplash.com/photo-1484723091739-30a097e8f929?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=387&q=80",
+                  }}
+                >
+                  <View style={{ position: "absolute", bottom: 20, left: 20 }}>
+                    <Text
+                      style={{
+                        fontSize: 14,
+                        color: "white",
+                        fontWeight: "bold",
+                      }}
+                    >
+                      Lorem Ipsum
+                    </Text>
+                    <Text
+                      style={{
+                        fontSize: 14,
+                        color: "white",
+                        fontWeight: "bold",
+                      }}
+                    >
+                      <Feather name="star" size={18} color="white" /> 5
+                    </Text>
+                  </View>
+                </ImageBackground>
+              </View>
+              <View
+                style={{
+                  overflow: "hidden",
+                  borderRadius: 10,
+                  marginRight: 10,
+                  maxHeight: 200,
+                }}
+              >
+                <ImageBackground
+                  style={{
+                    width: 150,
+                    height: 200,
+                  }}
+                  source={{
+                    uri:
+                      "https://images.unsplash.com/photo-1540189549336-e6e99c3679fe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80",
+                  }}
+                >
+                  <View style={{ position: "absolute", bottom: 20, left: 20 }}>
+                    <Text
+                      style={{
+                        fontSize: 14,
+                        color: "white",
+                        fontWeight: "bold",
+                      }}
+                    >
+                      Lorem Ipsum
+                    </Text>
+                    <Text
+                      style={{
+                        fontSize: 14,
+                        color: "white",
+                        fontWeight: "bold",
+                      }}
+                    >
+                      <Feather name="star" size={18} color="white" /> 5
+                    </Text>
+                  </View>
+                </ImageBackground>
+              </View>
+              <View
+                style={{
+                  overflow: "hidden",
+                  borderRadius: 10,
+                  marginRight: 10,
+                  maxHeight: 200,
+                }}
+              >
+                <ImageBackground
+                  style={{
+                    width: 150,
+                    height: 200,
+                  }}
+                  source={{
+                    uri:
+                      "https://images.unsplash.com/photo-1482049016688-2d3e1b311543?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=353&q=80",
+                  }}
+                >
+                  <View style={{ position: "absolute", bottom: 20, left: 20 }}>
+                    <Text
+                      style={{
+                        fontSize: 14,
+                        color: "white",
+                        fontWeight: "bold",
+                      }}
+                    >
+                      Lorem Ipsum
+                    </Text>
+                    <Text
+                      style={{
+                        fontSize: 14,
+                        color: "white",
+                        fontWeight: "bold",
+                      }}
+                    >
+                      <Feather name="star" size={18} color="white" /> 5
+                    </Text>
+                  </View>
+                </ImageBackground>
+              </View>
+              <View style={{ width: 30 }}></View>
+            </View>
+          </ScrollView>
+
+
+
           
         </ScrollView>
       </View>
